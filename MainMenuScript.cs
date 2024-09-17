8d51b302085a3fa4c8c346e7b9b927a56649980c
DanielMossichuk <paple124@gmail.com>
2024-09-17 19:59:44 +0300

    Daily Rewards System
    System for daily login and accepting rewards (coins).

---
+++ MainMenuScript.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // For accessing the UI elements like Button and Text
+using System.IO;
+using System.Collections;
+using TMPro;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [SerializeField] SaveManager saveManager;
+    [SerializeField] TextMeshProUGUI playButtonText; // Reference to the Text component of the Play button
+    private bool _hasSaveFile; // Boolean to track if a save file exists
+
+
+    private void Start()
+    {
+        //UpdatePlayButtonText();
+    }
+
+    public void PlayTheGameButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void SetInputButtons()
+    {
+        PlayerPrefs.SetInt("ButtonSelected", 1);
+        Debug.Log("buttons");
+    }
+    public void SetInputTouch()
+    {
+        PlayerPrefs.SetInt("ButtonSelected", 0);
+        Debug.Log("touch");
+    }
+    public void SetInputGyro()
+    {
+        PlayerPrefs.SetInt("ButtonSelected", 2);
+        Debug.Log("Gyroo");
+    }
+    public void OpenSetingsMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ShopMenu()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void OpenDailyMenu()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void MenuSwitcher(GameObject menu)
+    {
+        if (menu.activeInHierarchy)
+            menu.SetActive(false);
+        else
+            menu.SetActive(true);
+    }
+
+    #region << Continue Game >>
+    private void CheckForSaveFile()
+    {
+        string saveFilePath = Path.Combine(Application.persistentDataPath, "Save In Game Data.game");
+        Debug.Log($"Checking for save file at: {saveFilePath}");
+        _hasSaveFile = File.Exists(saveFilePath); // Set the boolean based on whether the file exists
+        Debug.Log($"Found Saved Data : {_hasSaveFile}");
+    }
+
+    private void UpdatePlayButtonText()
+    {
+        if (playButtonText != null)
+        {
+            playButtonText.text = "Continue";
+        }
+        else if (playButtonText != null)
+        {
+            playButtonText.text = "New Game";
+        }
+    }
+    #endregion
+}
